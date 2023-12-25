f8645b1c8cb1f8d49268a7686d86e0da6f0491e1
FumarinOG <mike@fumarin.com>
2023-12-24 18:45:11 -0600

    Updated NuGet packages

--- ObjectLibrary/obj/Debug/netstandard2.0/ObjectLibrary.AssemblyInfo.cs
+++ ObjectLibrary/obj/Debug/netstandard2.0/ObjectLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObjectLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603e66dd006d2780f99bc59ba4c2b445addd9282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObjectLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObjectLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
